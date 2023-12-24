318fe47b3b40e10c885b9399d0d2ff717ab67966
Muhammad Anas <anasamin26@gmail.com>
2023-12-25 00:08:57 +0500

    Postgress

--- carpoolbackend/carpoolproject/carpoolproject/settings.py
+++ carpoolbackend/carpoolproject/carpoolproject/settings.py
@@ -85,16 +85,19 @@ WSGI_APPLICATION = 'carpoolproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yourdbname',
-        'USER': 'postgres',
-        'PASSWORD': 'anas9235',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': '5432',  # You may adjust the port if needed
     }
 }
+
 
 
 AUTH_USER_MODEL = 'carpoolbackend.User'
